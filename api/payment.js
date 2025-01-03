8946c954c6e9e70e5a325393943087c2986f2653
ahmedredaali24 <158258893+ahmedredaali24@users.noreply.github.com>
2025-01-03 14:27:26 +0200

    Update payment.js

--- api/payment.js
+++ api/payment.js
@@ -1,5 +1,18 @@
-// api/payment.js
-module.exports = (req, res) => {
-    // يمكنك معالجة الدفع أو إجراء أي عملية هنا
-    res.status(200).json({ message: 'Payment processed successfully' });
-  };
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+module.exports = async (req, res) => {
+  if (req.method === 'POST') {
+    const { amount } = req.body;
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount * 100, // المبلغ بالـ سنتات
+        currency: 'usd',
+      });
+      res.status(200).json({ clientSecret: paymentIntent.client_secret });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  } else {
+    res.status(405).end(); // Method Not Allowed
+  }
+};
