ae6d9e6a4fa7dc368f9a6026fa9c5ebb52861ebd
ahmedredaali24 <158258893+ahmedredaali24@users.noreply.github.com>
2025-01-11 23:09:12 +0200

    Update payment.js

--- api/payment.js
+++ api/payment.js
@@ -1,39 +1,48 @@
 const functions = require("firebase-functions");
-const stripe = require('stripe')("sk_test_51QVEcuFZsKu3gKZXb2hKPiDmT34H1XMBfKqiaWtCJNRKUwabWiEVZWiHYSMGn7oxX5VrBwSDRiWhuG0VnK1GDyDV00jqQUy2Dp");
+const stripe = require("stripe")("sk_test_51QVEcuFZsKu3gKZXb2hKPiDmT34H1XMBfKqiaWtCJNRKUwabWiEVZWiHYSMGn7oxX5VrBwSDRiWhuG0VnK1GDyDV00jqQUy2Dp");
 
-exports.stripePaymentIntentRequest = functions.https.onRequest(async (requestAnimationFrame, res) => {
-  try {
-    let customerId;
-    const customerList = await stripe.customers.list({
-      email: req.body.email,
-      limit: 1
-    });
-    if (customerList.data.length !== 0) {
-      customerId = customerList.data[0].id;
+exports.stripePaymentIntentRequest = functions.https.onRequest(async (req, res) => {
+    try {
+        let customerId;
 
-    } else {
-      const customer = await stripe.customers.create({
-        email: req.body.email
-      });
-      customerId = customer.data.id;
+        //Gets the customer who's email id matches the one sent by the client
+        const customerList = await stripe.customers.list({
+            email: req.body.email,
+            limit: 1
+        });
+                
+        //Checks the if the customer exists, if not creates a new customer
+        if (customerList.data.length !== 0) {
+            customerId = customerList.data[0].id;
+        }
+        else {
+            const customer = await stripe.customers.create({
+                email: req.body.email
+            });
+            customerId = customer.data.id;
+        }
+
+        //Creates a temporary secret key linked with the customer 
+        const ephemeralKey = await stripe.ephemeralKeys.create(
+            { customer: customerId },
+            { apiVersion: '2020-08-27' }
+        );
+
+        //Creates a new payment intent with amount passed in from the client
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: parseInt(req.body.amount),
+            currency: 'usd',
+            customer: customerId,
+        })
+
+        res.status(200).send({
+            paymentIntent: paymentIntent.client_secret,
+            ephemeralKey: ephemeralKey.secret,
+            customer: customerId,
+            success: true,
+        })
+        
+    } catch (error) {
+        res.status(404).send({ success: false, error: error.message })
     }
-    const ephemeralKey = await stripe.ephemeralKey.create(
-      { customer: customerId }, { apiVersion: "2025-01-3" }
-    );
-    const paymentIntent = await stripe.paymentIntent.create({
-      amount: parsenInt(req.body.amount),
-      currency: "usd",
-      customer: customerId,
-    });
-    res.status(200).send({
-       paymentIntent: paymentIntent.client_secret,
-      ephemeralKey: ephemeralKey.secret,
-      customer: customerId,
-      success: true,
-    });
-  } catch (erorr) {
-    res.status(404).send({ success: false, erorr: erorr.message });
-  }
-}
-
-);
+});
